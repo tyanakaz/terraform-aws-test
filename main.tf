3691743dcda1a174d8222fe31e459c9bdcdc587a
tyanakaz <xe314m@gmail.com>
2019-06-22 01:56:24 +0900

    Create Network

--- main.tf
+++ main.tf
@@ -7,10 +7,3 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
 }
-
-resource "aws_vpc" "vpc-1" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "vpc-1"
-  }
-}
